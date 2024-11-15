d038821092592985a765b01649c495c613cdc4fc
Oğuz Kağnıcı <115665998+oguzkagnici@users.noreply.github.com>
2024-11-15 17:33:45 +0300

    Update eval_data.py for generic use cases.

--- eval_data.py
+++ eval_data.py
@@ -19,39 +19,36 @@ def count_matches(file_path, generated_password_set):
     return match_count, total_count
 
 # Load generated passwords into a set
-with open("generated_passwords2.txt", 'r', encoding='latin-1') as f:
+with open("generated_passwords.txt", 'r', encoding='latin-1') as f:
     generated_password_set = set(line.strip() for line in f)
 
-# Count matches in test_rockyou
-test_rockyou_matches, test_rockyou_total = count_matches("test_rockyou.txt", generated_password_set)
+# Count matches in test_data
+test_matches, test_total = count_matches("test_data.txt", generated_password_set)
 
-# Count matches in test_rockyou_no_hc
-#test_rockyou_no_hc_matches, test_rockyou_no_hc_total = count_matches("test_rockyou_no_hc.txt", generated_password_set)
+
 
 # Calculate percentages
-test_rockyou_percentage = (test_rockyou_matches / test_rockyou_total) * 100 if test_rockyou_total > 0 else 0
-#test_rockyou_no_hc_percentage = (test_rockyou_no_hc_matches / test_rockyou_no_hc_total) * 100 if test_rockyou_no_hc_total > 0 else 0
+test_percentage = (test_matches / test_total) * 100 if test_total > 0 else 0
 
 # Print the results
-print(f"Matches in test_rockyou: {test_rockyou_matches} ({test_rockyou_percentage:.2f}%) out of {test_rockyou_total}")
-#print(f"Matches in test_rockyou_no_hc: {test_rockyou_no_hc_matches} ({test_rockyou_no_hc_percentage:.2f}%) out of {test_rockyou_no_hc_total}")
+print(f"Matches in test_data: {test_matches} ({test_percentage:.2f}%) out of {test_total}")
 
 
 
-def count_matches_reverse(test_file_path, hashcat_file_path):
-    # Load test_rockyou passwords into a set
-    with open(test_file_path, 'r', encoding='latin-1') as f:
-        test_password_set = set(line.strip() for line in f)
-        total_count = len(test_password_set)
+def count_matches_reverse(generated_file_path, test_file_path):
+    
+    with open(generated_file_path, 'r', encoding='latin-1') as f:
+        generated_password_set = set(line.strip() for line in f)
+        total_count = len(generated_password_set)
 
     match_count = 0
     found_password_set = set()
 
     # Process the large file in a memory-efficient way
-    with open(hashcat_file_path, 'r', encoding='latin-1') as f:
-        for line in tqdm(f, desc='Checking passwords in hashcat_rockyou'):
+    with open(test_file_path, 'r', encoding='latin-1') as f:
+        for line in tqdm(f, desc=f"Checking passwords in {test_file_path}"):
             password = line.strip()
-            if password in test_password_set and password not in found_password_set:
+            if password in generated_password_set and password not in found_password_set:
                 match_count += 1
                 found_password_set.add(password)
 
